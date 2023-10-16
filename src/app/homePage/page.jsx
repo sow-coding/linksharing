f8e4986f2c8aa897819c2cb6c784315e486c8453
Saikou Sow <sow4work@gmail.com>
2023-10-17 00:56:30 +0200

    SaaS skeleton added

--- src/app/homePage/page.jsx
+++ src/app/homePage/page.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/navbar/navbar'
+import CustomLinks from '../components/customLinks/customLinks'
+import LinksContainer from '../components/linksContainer/linksContainer'
 
 function HomePage() {
   return (
     <div className='homePage'>
       <Navbar/>
+      <div className="linksApp">
+        <LinksContainer />
+        <CustomLinks />
+      </div>
     </div>
   )
 }
