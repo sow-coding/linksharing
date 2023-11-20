13b1057b9c627a57560fca1c6086777707ef5ab7
Saikou Sow <sow4work@gmail.com>
2023-11-20 10:51:28 +0100

    notifications issue solved

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/global.css'
 import { Inter } from 'next/font/google'
 
@@ -12,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
