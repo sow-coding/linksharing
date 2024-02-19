f58d5e8b1aac2b335f621150086613b502858af5
Saikou Sow <sow4work@gmail.com>
2024-02-19 12:15:50 +0100

    sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,10 +4,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://linkshare.fr',
+      lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
       url: 'https://linkshare.fr/login',
+      lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 0.2,
     }
   ]
